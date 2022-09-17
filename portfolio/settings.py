6ef750935213f44593eca9f02355a5b074523aae
Destiny <destinyeasyaccess@gmail.com>
2022-09-17 15:12:44 +0100

    'update'

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -20,14 +20,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = True
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG_VALUE')
 
 
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['destinyeasyaccess.herokuapp.com']
 
 
 # Application definition
