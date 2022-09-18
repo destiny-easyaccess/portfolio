f4456c01dc8d8296d3f41d1388dadc9f918777ad
Destiny <destinyeasyaccess@gmail.com>
2022-09-18 02:40:47 +0100

    settings

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -124,11 +124,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 
+MEDIA_URL = 'images/'
+MEDIA_ROOT = BASE_DIR / 'static/images'
 
-MEDIA_URL = '/images/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 
 
 
